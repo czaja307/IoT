98a8d0f5179542c9f467b3c071b5339861243a9b
Anna Sidor <annasidor00@gmail.com>
2024-12-10 22:51:54 +0100

    checkout module added

--- raspberry/common/interactions_interface.py
+++ raspberry/common/interactions_interface.py
@@ -3,16 +3,41 @@ from abc import ABC, abstractmethod
 class InteractionsInterface(ABC):
     def __init__(self):
         self._quit_action = None
+        self._confirm_action = None
+        self._cancel_action = None
 
     def assign_quit_action(self, action):
         self._quit_action = action
 
+    def assign_confirm_action(self, action):
+        self._confirm_action = action
+
+    def assign_cancel_action(self, action):
+        self._cancel_action = action
+
     def quit_sig_sent(self):
-        print("U STAREGO")
         if self._quit_action:
             try:
                 self._quit_action()
             except Exception as e:
-                print(f"Exception in _quit_action: {e}")
+                print(f"Exception in quit_action: {e}")
         else:
             print("No quit action assigned.")
+
+    def confirm_sig_sent(self):
+        if self._confirm_action:
+            try:
+                self._confirm_action()
+            except Exception as e:
+                print(f"Exception in confirm_action: {e}")
+        else:
+            print("No confirm action assigned.")
+
+    def cancel_sig_sent(self):
+        if self._cancel_action:
+            try:
+                self._cancel_action()
+            except Exception as e:
+                print(f"Exception in cancel_action: {e}")
+        else:
+            print("No cancel action assigned.")
