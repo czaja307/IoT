c3f75424659e51274e496514b7d838c729afaedb
Jakub Czajkowski <and.jj.an@gmail.com>
2025-01-14 00:36:07 +0100

    refactor(server-communication): assign products to terminal
    Signed-off-by: Jakub Czajkowski <and.jj.an@gmail.com>

--- backend/raspberry_interactions/raspberry_msg_handling.py
+++ backend/raspberry_interactions/raspberry_msg_handling.py
@@ -1,14 +1,16 @@
 from controllers.tag import create_tag, update_tag, get_tag
+from raspberry_interactions import ServerCommunications
 from schemas.tag import TagCreate, TagUpdate
 from database import get_db
-from models.product import Product as mProduct
 from schemas.product import Product as sProduct
+
 
 class RaspberryMsgHandling:
     @staticmethod
     def on_terminal_msg(msg):
         tag = int(msg)
         prod = 2
+        terminal = 1
         get_db_v = get_db()
         db = next(get_db_v)
         tag_from_db = get_tag(db, tag)
@@ -16,7 +18,7 @@ class RaspberryMsgHandling:
             update_tag(db, tag, TagUpdate(id=tag, product_id=prod))
         else:
             create_tag(db, TagCreate(id=tag, product_id=prod))
-
+        ServerCommunications().terminals_products_dict[terminal] = prod
 
     @staticmethod
     def on_checkout_msg(msg):
