b478baee167b51f2d5ba952e36eed069f1c5bc51
Jakub Czajkowski <38287974+czaja307@users.noreply.github.com>
2025-01-14 14:39:33 +0100

    feat: multiple terminals support (#9)
    * feat(annotations): singleton annotation
    
    * feat(server-communication): store the list of subscribed devices
    
    * refactor(server-communication): simplify logic
    
    * fix(server-communication): registered_count not updating properly
    
    * refactor(server-communication): add some typing
    
    * feat(terminal): add terminal schemas
    
    * refactor(terminal): update naming convention
    
    * feat(terminal): add terminal endpoints
    
    * refactor(server-communication): assign products to terminal
    
    Signed-off-by: Jakub Czajkowski <and.jj.an@gmail.com>
    
    * refactor(server-communication): simplify logic for dict initialization
    
    * refactor(server-communication): assign products to terminal
    
    ---------
    
    Signed-off-by: Jakub Czajkowski <and.jj.an@gmail.com>

--- backend/raspberry_interactions/raspberry_msg_handling.py
+++ backend/raspberry_interactions/raspberry_msg_handling.py
@@ -1,14 +1,19 @@
 from controllers.tag import create_tag, update_tag, get_tag
+from raspberry_interactions import ServerCommunications
 from schemas.tag import TagCreate, TagUpdate
 from database import get_db
-from models.product import Product as mProduct
 from schemas.product import Product as sProduct
+
 
 class RaspberryMsgHandling:
     @staticmethod
-    def on_terminal_msg(msg):
+    def on_terminal_msg(msg, topic):
         tag = int(msg)
-        prod = 2
+        terminal_id = int(topic.split("/")[2])
+        prod = int(ServerCommunications().terminals_products_dict[terminal_id])
+        if not prod:
+            return
+
         get_db_v = get_db()
         db = next(get_db_v)
         tag_from_db = get_tag(db, tag)
@@ -16,7 +21,6 @@ class RaspberryMsgHandling:
             update_tag(db, tag, TagUpdate(id=tag, product_id=prod))
         else:
             create_tag(db, TagCreate(id=tag, product_id=prod))
-
 
     @staticmethod
     def on_checkout_msg(msg):
--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from routers import product, tag, purchase
+from routers import product, tag, purchase, terminal
 
 router = APIRouter()
 router.include_router(product.router, prefix="/products", tags=["products"])
 router.include_router(tag.router, prefix="/tags", tags=["tags"])
 router.include_router(purchase.router, prefix="/purchases", tags=["purchases"])
+router.include_router(terminal.router, prefix="/terminals", tags=["terminals"])
